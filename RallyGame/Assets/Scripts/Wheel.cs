aabd4ba385e1f54c78ef1349e13b6592ce21b452
And5666 <andrew.collin.uk@outlook.com>
2022-10-15 20:31:04 +0100

    Update Wheel.cs

--- RallyGame/Assets/Scripts/Wheel.cs
+++ RallyGame/Assets/Scripts/Wheel.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Wheel : MonoBehaviour
 {
     public Rigidbody rb;
+    [Header("Grip and Traction")]
+    public float friction;
+
+    private float rollingResistance;
+
 
     [Header("Suspension")]
     public float springTravel;              //max length the spring can travel
@@ -30,16 +35,29 @@ public class Wheel : MonoBehaviour
 
     void FixedUpdate()
     {
-        if(Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, springTravel + wheelRadius))              //send raycast out from the wheel position
+        if (Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, springTravel + wheelRadius))
         {
-            lastLength = springLength;                                                  //set spring length
-            springLength = hit.distance - wheelRadius;                                                //calculate current spring length
-            springVelocity = (lastLength - springLength) / Time.fixedDeltaTime;         //calculate the rate of change of spring length
-            springForce = springStriffness * (springTravel - springLength);
-            damperForce = damperStiffness * springVelocity;                             
+            suspension(hit);
 
-            suspensionForce = (springForce + damperForce) * transform.up;
-            rb.AddForceAtPosition(suspensionForce, transform.position);
         }
+    }
+
+    void suspension(RaycastHit hit)
+    {
+        lastLength = springLength;                                                  //set spring length
+        springLength = hit.distance - wheelRadius;                                  //calculate current spring length
+        springVelocity = (lastLength - springLength) / Time.fixedDeltaTime;         //calculate the rate of change of spring length
+        springForce = springStriffness * (springTravel - springLength);
+        damperForce = damperStiffness * springVelocity;
+
+        suspensionForce = (springForce + damperForce) * transform.up;
+        rb.AddForceAtPosition(suspensionForce, transform.position);
+    }
+
+    void forwardForces(RaycastHit hit)  
+    {
+        Vector3 wheelVelocity = transform.InverseTransformDirection(rb.GetPointVelocity(hit.point)); 
+
+
     }
 }
