3a1cbb8bc5e9c1be04dd71cf4ef36b66cb6fc48a
And5666 <andrew.collin.uk@outlook.com>
2022-10-20 23:47:45 +0100

    physics kind of working now
    i want to die

--- RallyGame/Assets/Scripts/CarController.cs
+++ RallyGame/Assets/Scripts/CarController.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody rb;
+    public Transform forcePoint;
+
+    public Wheel driveWheelRight;
+    public Wheel driveWheelLeft;
+
+    [Header("Divechain Variables")]
+    public AnimationCurve engineCurve;
+    public float differential;
+    public float transmissionEfficiency;
+    public float[] gear;
+    public float driveTorque;
+
+    public float rpm;
+    public int i;
+
+    float x;
+    float y;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        enginePower();
+        inputs();
+    }
+
+    void FixedUpdate()
+    {
+        //apply air resistance
+    }
+
+    void enginePower()
+    {
+        rpm = (rb.velocity.magnitude / driveWheelLeft.wheelRadius) * gear[i] * differential * 60 / (2 * Mathf.PI);
+        if (rpm < 1000)
+        {
+            rpm = 1000;
+        }
+        if(x > 0)
+        {
+            driveTorque = engineCurve.Evaluate(rpm) * gear[i] * differential * transmissionEfficiency * x;
+        }
+    }
+
+
+
+    void inputs()
+    {
+        x = Input.GetAxis("Vertical");
+        y = Input.GetAxis("Horizontal");
+    }
+
+    void shiftUp()
+    {
+        i++;
+    }
+
+    void shiftDown()
+    {
+        i--;
     }
 }
--- RallyGame/Assets/Scripts/Wheel.cs
+++ RallyGame/Assets/Scripts/Wheel.cs
@@ -5,32 +5,51 @@ using UnityEngine;
 public class Wheel : MonoBehaviour
 {
     public Rigidbody rb;
+    public CarController controller;
+
+    [Header("Drive Type")]
+    public bool fwd;
+    public bool rwd;
+    public bool awd;
+
     [Header("Grip and Traction")]
-    public float friction;
+    public AnimationCurve slipCurve;
+    public float rollingResistance;
+    public float brakeForce;
 
-    private float rollingResistance;
-
+    public float brakeTorque;
+    public float slipRatio;
+    private float weightOnWheel;
+    public float angularVelocity;
+    private Vector3 wheelVelocity;
 
     [Header("Suspension")]
-    public float springTravel;              //max length the spring can travel
+    public float springTravel;              
     public float springStriffness;          
     public float damperStiffness;
 
     private float springLength;             
     private float springForce;             
 
-    private float damperForce;              //current damper force
-    private float springVelocity;           //current spring velocity
-    private float lastLength;               //length of the spring the previous frame
+    private float damperForce;            
+    private float springVelocity;          
+    private float lastLength;               
 
-    private Vector3 suspensionForce;        //spring force applied in upwards direction
+    private Vector3 suspensionForce;       
 
     [Header("Wheel")]
     public GameObject wheelModel;           
     public float wheelRadius;
+    public float wheelMass;
+    public bool isFront;
 
     void Start()
     {
+    }
+
+    void Update()
+    {
+        //spin wheel model
     }
 
     void FixedUpdate()
@@ -38,15 +57,17 @@ public class Wheel : MonoBehaviour
         if (Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, springTravel + wheelRadius))
         {
             suspension(hit);
-
+            calculateWeight(hit);
+            applyForces(hit);
+            calcualteSlipRatio();
         }
     }
 
     void suspension(RaycastHit hit)
     {
-        lastLength = springLength;                                                  //set spring length
-        springLength = hit.distance - wheelRadius;                                  //calculate current spring length
-        springVelocity = (lastLength - springLength) / Time.fixedDeltaTime;         //calculate the rate of change of spring length
+        lastLength = springLength;                                                  
+        springLength = hit.distance - wheelRadius;                                  
+        springVelocity = (lastLength - springLength) / Time.fixedDeltaTime;         
         springForce = springStriffness * (springTravel - springLength);
         damperForce = damperStiffness * springVelocity;
 
@@ -54,10 +75,68 @@ public class Wheel : MonoBehaviour
         rb.AddForceAtPosition(suspensionForce, transform.position);
     }
 
-    void forwardForces(RaycastHit hit)  
+    void applyForces(RaycastHit hit)
     {
-        Vector3 wheelVelocity = transform.InverseTransformDirection(rb.GetPointVelocity(hit.point)); 
+        wheelVelocity = transform.InverseTransformDirection(rb.GetPointVelocity(hit.point));
+        Vector3 rollingDrag = -rollingResistance * wheelVelocity;
 
+        if(Input.GetAxis("Vertical") < 0)
+        {
+            brakeTorque = brakeForce * wheelRadius * Input.GetAxis("Vertical");
+        }
+        else
+        {
+            brakeTorque = 0;
+        }
 
+        if (fwd)
+        {
+            if (isFront) 
+            {
+                float finalTorque = controller.driveTorque - brakeTorque; 
+                angularVelocity = finalTorque / (wheelMass * wheelRadius * wheelRadius / 2);
+                rb.AddForceAtPosition(slipCurve.Evaluate(slipRatio) / 2 * transform.forward, hit.point);
+            }
+            else 
+            {
+                float finalTorque = -brakeTorque;
+                angularVelocity = finalTorque / (wheelMass * wheelRadius * wheelRadius / 2);
+                // add brake torque
+            }
+            //front wheel drive
+        }
+        else if(rwd)
+        {
+            if (!isFront)
+            {
+                float finalTorque = - brakeTorque;
+                angularVelocity = finalTorque / (wheelMass * wheelRadius * wheelRadius / 2);
+                rb.AddForceAtPosition(slipCurve.Evaluate(slipRatio) * transform.forward, hit.point);
+            }
+            else
+            {
+                float finalTorque = controller.driveTorque - brakeTorque;
+                angularVelocity = finalTorque / (wheelMass * wheelRadius * wheelRadius / 2);
+                //add brake torque
+            }
+            //rear wheel dirve
+        }
+        else if(awd)
+        {
+            float finalTorque = controller.driveTorque - brakeTorque;
+            angularVelocity = finalTorque / (wheelMass * wheelRadius * wheelRadius / 4);
+            rb.AddForceAtPosition(slipCurve.Evaluate(slipRatio) * transform.forward / 4, hit.point);
+        }
+    }
+
+    void calculateWeight(RaycastHit hit)
+    {
+        float springPercentage = hit.distance / (springTravel + wheelRadius);
+        weightOnWheel = (rb.mass / springPercentage) / 4;
+    }
+
+    void calcualteSlipRatio()
+    {
+        slipRatio = ((angularVelocity * wheelRadius) / wheelVelocity.magnitude);
     }
 }
